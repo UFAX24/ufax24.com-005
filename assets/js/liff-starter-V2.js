8a405a4282119a0e6729655c75cb2b6474dbe5b9
UFAX24 <87823194+UFAX24@users.noreply.github.com>
2021-07-23 10:17:46 +0700

    Update liff-starter-V2.js

--- assets/js/liff-starter-V2.js
+++ assets/js/liff-starter-V2.js
@@ -2,7 +2,7 @@
 Author :
 - https://github.com/crash-override404
 */
-var liffId = "1656198214-WyJRPLlB";
+var liffId = "1656244440-5eny0DJK";
 var params = location.search.substring(1);
 var url = window.location.href;
 if (params) {
